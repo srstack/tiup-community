981b3f914f8589337babba0f775cc3d08e205781
Allen Zhong <zhongbenli@pingcap.com>
2022-10-22 18:07:31 +0800

    pkg/papermc: use bare jar to start

--- packages/papermc/start.sh
+++ packages/papermc/start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [[ "$1" != "-p" ]]; then
-    WORKDIR=${TIUP_COMPONENT_DATA_DIR:-$HOME/.tiup}/storage/papermc
-    mkdir -p $WORKDIR
-    cd $WORKDIR
-fi
+WORKDIR=${TIUP_COMPONENT_DATA_DIR:-$HOME/.tiup}/storage/papermc
+mkdir -p $WORKDIR
+cd $WORKDIR
 
-exec "$TIUP_COMPONENT_INSTALL_DIR/papermc" "$@"
+exec java -jar "$TIUP_COMPONENT_INSTALL_DIR/papermc.jar" "$@"
